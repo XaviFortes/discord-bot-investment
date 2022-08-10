e8b4b84bb4449029fbf73fde454891580dbba64c
xavifortes <itsfortes@gmail.com>
2022-08-10 16:50:37 +0200

    Update kick to see changes

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -3,10 +3,10 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('kick')
-		.setDescription('Select a member and kick them (but not really).')
+		.setDescription('Select a member and kick them from vc.')
 		.addUserOption(option => option.setName('target').setDescription('The member to kick')),
 	async execute(interaction) {
 		const member = interaction.options.getMember('target');
-		return interaction.reply({ content: `You wanted to kick: ${member.user.username}`, ephemeral: true });
+		return interaction.reply({ content: `You wanted to kick: ${member.user} ${member.user.username}`, ephemeral: true });
 	},
 };
