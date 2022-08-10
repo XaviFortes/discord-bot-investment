bcec2a19221272de0ba294248f55b6c8a824076b
xavifortes <itsfortes@gmail.com>
2022-08-10 17:11:29 +0200

    Implemented call to the API

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -6,8 +6,34 @@ module.exports = {
 		.setDescription('Select a member and kick them from vc.')
 		.addUserOption(option => option.setName('target').setDescription('The member to kick')),
 	async execute(interaction) {
-		const member = interaction.options.getMember('target');
-		member.voice.disconnect();
-		return interaction.reply({ content: `You wanted to kick: ${member} ${member.user} ${member.user.username}`, ephemeral: true });
+		const {
+            statusCode,
+            body
+
+         } = await request(`http://${ip}:8280/api/discord/kick`, {
+            method: 'POST',
+            //path: '/api/discord/user',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: interaction.user.id,
+                username: interaction.user.username
+            }),
+        })
+        for await (const data of body) {
+            console.log('data', data)
+            // Parse the JSON data and return it.
+            const parsedData = JSON.parse(data)
+            const message = parsedData.message
+			if (message === 'ok') {
+				const member = interaction.options.getMember('target');
+				member.voice.disconnect();
+				return interaction.reply({ content: 'Successfully kicked member.', ephemeral: true });
+			}
+            return interaction.reply(`${message}`);
+        }
+		//return interaction.reply({ content: `You wanted to kick: ${member} ${member.user} ${member.user.username}`, ephemeral: true });
 	},
 };
